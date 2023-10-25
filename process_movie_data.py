6311805ccfa293cecb91f82d43f9e45ff32e0bd4
Hannah Guo <hannag@Hannahs-MacBook-Pro-6.local>
2023-10-25 09:17:31 -0700

    changed function to top_N (with 10 as default val)

--- process_movie_data.py
+++ process_movie_data.py
@@ -4,7 +4,7 @@ import csv
 
 
 
-def find_top_n(filename, N=5):
+def find_top_n(filename, N=10):
     """Finds the top n highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
